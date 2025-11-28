e8f4f1dd349d5efe13ad87e642a61b7e2bd9d1d5
FelZimmer <fozimermann@gmail.com>
2025-11-28 18:53:47 -0300

    fix: remove unused image elements from FundoT component

--- src/Components/FundoTotem/FundoT.jsx
+++ src/Components/FundoTotem/FundoT.jsx
@@ -6,9 +6,7 @@ export default function FundoT() {
   return (
     <>
       
-      <img  src="/public/images/img/Vector (3).png" alt="" className="geometric-image top-right"/>
-
-      <img src="/public/images/img/Vector (4).png" alt="" className="geometric-image bottom-left"/>
+      
 </>
   );
 }
